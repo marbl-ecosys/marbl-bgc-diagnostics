74e383b58f2a92ef95924672abdd344d91f1b0f1
Max Grover <mgroverwx@gmail.com>
2021-09-24 09:34:29 -0600

    add updated config file

--- notebooks/config.py
+++ notebooks/config.py
@@ -2,3 +2,6 @@ import yaml
 
 with open("analysis_config.yml", mode="r") as fptr:
     analysis_config = yaml.safe_load(fptr)
+
+analysis_config['all_variables'] = analysis_config['variables']['physics'] + analysis_config['variables']['biogeochemistry']
+analysis_config['all_cases'] = [analysis_config['reference_case_name']] + analysis_config['compare_case_name'] 
